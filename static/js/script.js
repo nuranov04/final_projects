ea57966681065c437ef6d1300a775f8e30ca2e4f
artur <80958429+nuranov04@users.noreply.github.com>
2022-11-04 14:43:32 +0600

    Delete static directory

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-const $dropdownButtonBtn = document.querySelector('.dropdownButtonBtn');
-
-const $dropdownButtonDiv = document.querySelector('.dropdownButtonDiv');
-
-$dropdownButtonBtn.addEventListener('click' , e => {
-    e.preventDefault();
-
-    $dropdownButtonDiv.classList.toggle('displayNone')
-})
